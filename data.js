1e77df3367a1d82455fd24d04e9438638dafa9a9
SaravanaKumar M <82089574+SaravanaKumarVM@users.noreply.github.com>
2026-01-11 10:30:47 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,3 +3,7 @@ let entries = JSON.parse(localStorage.getItem("entries") || "[]");
 function saveData(){
   localStorage.setItem("entries", JSON.stringify(entries));
 }
+
+function today(){
+  return new Date().toISOString().slice(0,10);
+}
